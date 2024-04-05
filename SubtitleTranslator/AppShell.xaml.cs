34ccf21f8dbb525660c01a9e1ecffc79afdb4a19
qifu1177 <qifu1177@gmail.com>
2024-04-05 15:07:59 +0200

    update icon

--- SubtitleTranslator/AppShell.xaml.cs
+++ SubtitleTranslator/AppShell.xaml.cs
@@ -1,4 +1,8 @@
-﻿using App.UI.Infrastructure.Datas;
+﻿using App.Infrastructure.Common;
+using App.Infrastructure.Interfaces.Services;
+using App.UI.Infrastructure.Datas;
+using App.UI.Infrastructure.Services;
+using SubtitleTranslator.Resources;
 using SubtitleTranslator.ViewModels;
 
 namespace SubtitleTranslator
@@ -6,22 +10,27 @@ namespace SubtitleTranslator
     public partial class AppShell : Shell
     {
         private MainViewModel _viewModel;
-        public AppShell(MainViewModel viewModel)
+        private ISettingSerivice _settingSerivice;
+        private PathHelp _pathHelp;
+        
+        public AppShell(MainViewModel viewModel, ISettingSerivice settingSerivice, PathHelp pathHelp)
         {
             InitializeComponent();
             BindingContext = viewModel;
             _viewModel = viewModel;
+            _settingSerivice = settingSerivice;
+            _pathHelp = pathHelp;
+            Disappearing += AppShell_Disappearing;
         }
 
-        private void AppShell_SizeChanged(object? sender, EventArgs e)
+        private void AppShell_Disappearing(object? sender, EventArgs e)
         {
-            var view = sender;
+            var uiSetting = InstanceMap<UiSetting>.Instance;
+            if (uiSetting == null)
+                return;
+            string settingPath = System.IO.Path.Combine(_pathHelp.UserPath, ConstantValues.SETTINGFILE);
+            _settingSerivice.SaveUserSetting(uiSetting.UserSetting, settingPath);
         }
-
-        public void SetUiSetting(UiSetting setting)
-        {
-            _viewModel.SettingViewModel.SetData(setting);
-
-        }
+        
     }
 }
